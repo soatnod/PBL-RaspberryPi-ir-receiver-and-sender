0131e6f3882b1c92267f8d7431443df600debf89
soatnod <danielyi321@gmail.com>
2026-07-13 16:24:26 +0900

    Refactor temperature and humidity monitoring logic

--- temp_test.py
+++ temp_test.py
@@ -3,9 +3,10 @@ import board
 import time
 import RPi.GPIO as GPIO
 
+import send_signal as SS
+
 #センサータイプとピン番号を指定
 dht_device = adafruit_dht.DHT11(board.D3)
-
 #set LED pin
 pin_out_green = 18
 pin_out_red = 12
@@ -15,49 +16,55 @@ GPIO.setup(pin_out_red, GPIO.OUT)
 
 print("センサーの監視を開始します...")
 print("※終了するにはCtrl + Cを押してください。")
-
 temperature = 0
 humidity = 0
 
-hot_temp = 26
-cold_temp = 22
+hot_temp = 29
+cold_temp = 20
 
-while True:
-    try:
+try:
+    while True:
+        try:
+            if temperature is not None and humidity is not None:
+                temperature = dht_device.temperature
+                humidity = dht_device.humidity
 
-        if temperature is not None and humidity is not None:
-            temperature = dht_device.temperature
-            humidity = dht_device.humidity
+                print(f"温度：{temperature:.1f}℃　湿度：{humidity:.1f}%")
+                
+                Msnal = temperature - (temperature-10)*(0.8-humidity/100)/2.3 
+                
+                print(f"体感温度:{Msnal:.1f}℃")
+                
+                if Msnal>=hot_temp:
+                    print("体感温度が高くなっています。冷房をつけます。")
+                    GPIO.output(pin_out_green, GPIO.HIGH)
+                    GPIO.output(pin_out_red, GPIO.LOW)
+                    SS.sendSignal("cooler", 22, "on")
+                elif Msnal<=cold_temp:
+                    print("体感温度が低くなっています。暖房をつけてます。")
+                    GPIO.output(pin_out_red, GPIO.HIGH)
+                    GPIO.output(pin_out_green, GPIO.LOW)
+                    SS.sendSignal("heater", 27, "on")
+                else:
+                    GPIO.output(pin_out_red, GPIO.LOW)
+                    GPIO.output(pin_out_green, GPIO.LOW)
+                    
+                    print(f"適温です。エアコンをつける必要はありません。")
 
-            print(f"温度：{temperature:.1f}℃　湿度：{humidity:.1f}%")
+        except RuntimeError as error:
+            #読み取り失敗エラーをキャッチして無視し、処理を続行
+            print(f"読み取りエラー：{error.args[0]}")
             
-            Msnal = temperature - (temperature - 10) * (0.8 - humidity / 100) / 2.3
-            
-            print(f"体感温度:{Msnal:.1f}℃")
+            continue
 
-        if Msnal >= hot_temp:
-            GPIO.output(pin_out_green, GPIO.HIGH)
-        elif Msnal <= cold_temp:
-            GPIO.output(pin_out_red, GPIO.HIGH)
-        else:
-            GPIO.output(pin_out_green, GPIO.LOW)
-            GPIO.output(pin_out_red, GPIO.LOW)
+        except Exception as error:
+            #プログラムを強制終了するような深刻なエラーが起きた場合はセンサを開放
+            dht_device.exit()
+            raise error
         
-            print(f"温度は丁度いい。エアコンをつける必要がない。")
-        
-        
-        
-            
-            
-    except RuntimeError as error:
-        #読み取り失敗エラーをキャッチして無視し、処理を続行
-        print(f"読み取りエラー：{error.args[0]}")
-        continue
-
-    except Exception as error:
-        #プログラムを強制終了するような深刻なエラーが起きた場合はセンサを開放
-        dht_device.exit()
-        raise error
-
-    time.sleep(2.5)
-
+        time.sleep(60)
+    
+except KeyboardInterrupt:
+    pass
+print("CLEANUP")
+GPIO.cleanup()
